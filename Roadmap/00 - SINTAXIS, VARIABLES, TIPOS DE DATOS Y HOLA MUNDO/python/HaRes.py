dbaf5ba960ba6397a06064d4010cfd0714324944
HaR3s <zeidel07.gatcia@gmail.com>
2024-11-21 14:27:48 -0500

    ejercicio 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/HaRes.py
@@ -0,0 +1,44 @@
+#!/data/data/com.termux/files/usr/bin/python3
+"""
+* EJERCICIO:
+* - Crea un comentario en el código y coloca la URL del sitio web oficial del
+*   lenguaje de programación que has seleccionado.
+* - Representa las diferentes sintaxis que existen de crear comentarios
+*   en el lenguaje (en una línea, varias...).
+* - Crea una variable (y una constante si el lenguaje lo soporta).
+* - Crea variables representando todos los tipos de datos primitivos
+*   del lenguaje (cadenas de texto, enteros, booleanos...).
+* - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+"""
+
+# https://www.python.org/
+
+# Comentario de una linea
+
+"""
+Comentario multilinea
+del lenguaje Python
+usando triple comillas dobles
+"""
+
+# variable en Python
+var = None
+
+# Constante en Python
+PI = 3.141592
+
+# Variables primitivas
+
+# String
+texto = "Python3"
+
+# Int
+enteros = 10
+
+# Float
+decimales = 1.5
+
+# Buleanos
+verdadero = True
+
+print(f"Hola {texto}")
